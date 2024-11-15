284e58067a569085c9650896234ad5361ac35a37
Saulo Jose Coelho de Figueiredo <saulojcf@gmail.com>
2024-11-15 16:24:38 -0300

    Rotas de listagem e criação

--- src/app.js
+++ src/app.js
@@ -2,8 +2,30 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+const livros = [
+    {
+        id: 1,
+        titulo: 'Senhor dos Anéis'
+    },
+    {
+        id: 2,
+        titulo: 'O hobbit'
+    }
+];
+
 app.get('/', (req, res) => {
     res.status(200).send('Curso de Express API');
+});
+
+app.get('/livros', (req, res) => {
+    res.status(200).json(livros);
+});
+
+app.post('/livros', (req, res) => {
+    livros.push(req.body);
+    res.status(201).send('Livro cadastrado com sucesso!');
 });
 
 export default app;
