62d27cb0b98c182eaa01befc3d9f9623a38b80b2
Saulo Jose Coelho de Figueiredo <saulojcf@gmail.com>
2024-11-15 16:39:23 -0300

    Rotas get e put

--- src/app.js
+++ src/app.js
@@ -23,9 +23,26 @@ app.get('/livros', (req, res) => {
     res.status(200).json(livros);
 });
 
+app.get('/livros/:id', (req, res) => {
+    const index = buscaLivro(req.params.id);
+    res.status(200).json(livros[index]);
+});
+
 app.post('/livros', (req, res) => {
     livros.push(req.body);
     res.status(201).send('Livro cadastrado com sucesso!');
 });
+
+app.put('/livros/:id', (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.status(200).json(livros[index]);
+});
+
+function buscaLivro(id) {
+    return (
+        livros.findIndex((livro) => livro.id === Number(id))
+    );
+}
 
 export default app;
