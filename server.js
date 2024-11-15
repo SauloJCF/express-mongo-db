fc7a3d545f18df14c7e2238c1629b318ef5556e7
Saulo Jose Coelho de Figueiredo <saulojcf@gmail.com>
2024-11-15 15:34:21 -0300

    Configuracao Express

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-import http from 'http';
+import app from './src/app.js';
 
 const PORT = 3000;
 
-const rotas = {
-    "/": "Curso de Express.js API",
-    "/livros": "Acessou rota de livros",
-    "/autores": "Acessou rota de autores"
-}
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end(rotas[req.url]);
-});
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
-})
+});
