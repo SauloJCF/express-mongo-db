2f6b5e29c3e37831341bbbdfa1589ffbf646ecac
Saulo Jose Coelho de Figueiredo <saulojcf@gmail.com>
2024-11-20 17:25:28 -0300

    Post livros

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -4,5 +4,6 @@ import LivroController from '../controller/livroController.js';
 const routes = express.Router();
 
 routes.get('/livros', LivroController.listarLivros);
+routes.post('/livros', LivroController.cadastrarLivro);
 
 export default routes;
