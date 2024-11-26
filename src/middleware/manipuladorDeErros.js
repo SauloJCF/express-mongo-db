2aac75071678dd03299385194f53548a703b32c4
Saulo Jose Coelho de Figueiredo <saulojcf@gmail.com>
2024-11-26 13:49:45 -0300

    Classes de erro personalizadas e tratamento encapsulado

--- src/middleware/manipuladorDeErros.js
+++ src/middleware/manipuladorDeErros.js
@@ -1,27 +1,18 @@
 import mongoose from "mongoose";
+import ErroBase from "../erros/ErroBase.js";
+import RequisicaoIncorreta from "../erros/RequisicaoIncorreta.js";
+import ErroValidacao from "../erros/ErroValidacao.js";
 
 // eslint-disable-next-line no-unused-vars
 function manipuladorDeErros(erro, req, res, next) {
   console.log(erro);
 
   if (erro instanceof mongoose.Error.CastError) {
-    res.status(400).json({
-      message: "Um ou mais dados informados estão incorretos.",
-    });
+    new RequisicaoIncorreta().enviarResposta(res);
   } else if (erro instanceof mongoose.Error.ValidationError) {
-    const mensagensErro = Object.values(erro.errors)
-      .map((erro) => erro.message)
-      .join("; ");
-
-    res
-      .status(400)
-      .json({
-        message: `Os seguintes erros foram encontrados: ${mensagensErro}`,
-      });
+    new ErroValidacao(erro).enviarResposta(res);
   } else {
-    res.status(500).json({
-      message: "Erro interno de servidor.",
-    });
+    new ErroBase().enviarResposta(res);
   }
 }
 
