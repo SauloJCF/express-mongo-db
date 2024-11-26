68a4505fee049de6928373f058937256cc2e6773
Saulo Jose Coelho de Figueiredo <saulojcf@gmail.com>
2024-11-26 12:57:37 -0300

    Erros de validação com mensagem personalizada

--- src/middleware/manipuladorDeErros.js
+++ src/middleware/manipuladorDeErros.js
@@ -8,12 +8,21 @@ function manipuladorDeErros(erro, req, res, next) {
     res.status(400).json({
       message: "Um ou mais dados informados estão incorretos.",
     });
-    return;
-  }
+  } else if (erro instanceof mongoose.Error.ValidationError) {
+    const mensagensErro = Object.values(erro.errors)
+      .map((erro) => erro.message)
+      .join("; ");
 
-  res.status(500).json({
-    message: "Erro interno de servidor.",
-  });
+    res
+      .status(400)
+      .json({
+        message: `Os seguintes erros foram encontrados: ${mensagensErro}`,
+      });
+  } else {
+    res.status(500).json({
+      message: "Erro interno de servidor.",
+    });
+  }
 }
 
 export default manipuladorDeErros;
--- src/models/Livros.js
+++ src/models/Livros.js
@@ -1,14 +1,21 @@
 import mongoose from "mongoose";
 
-const livroSchema = new mongoose.Schema({
+const livroSchema = new mongoose.Schema(
+  {
     id: { type: mongoose.Schema.Types.ObjectId },
-    titulo: { type: String, required: true },
-    editora: { type: String },
+    titulo: { type: String, required: [true, "O título é obrigatório"] },
+    editora: { type: String, required: [true, "A editora é obrigatória"] },
     preco: { type: Number },
     paginas: { type: Number },
-    autor: {type: mongoose.Schema.Types.ObjectId, ref: 'autores', required: true},
-}, { versionKey: false });
+    autor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "autores",
+      required: [true, "O(a) autor(a) é obrigatório"],
+    },
+  },
+  { versionKey: false }
+);
 
-const livro = mongoose.model('livros', livroSchema);
+const livro = mongoose.model("livros", livroSchema);
 
 export default livro;
