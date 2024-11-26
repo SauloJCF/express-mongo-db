54193a31bbe7bb30d8e81b384f564c39bb66edf7
Saulo Jose Coelho de Figueiredo <saulojcf@gmail.com>
2024-11-26 12:36:01 -0300

    Log error

--- src/middleware/manipuladorDeErros.js
+++ src/middleware/manipuladorDeErros.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 // eslint-disable-next-line no-unused-vars
 function manipuladorDeErros(erro, req, res, next) {
+  console.log(erro);
+
   if (erro instanceof mongoose.Error.CastError) {
     res.status(400).json({
       message: "Um ou mais dados informados estão incorretos.",
